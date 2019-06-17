0632805473c7b40d2f42efab4a320e3f858bff95
miigotu <miigotu@gmail.com>
2019-06-17 06:28:59 -0700

    Break webserve into smaller more manageable files

---
+++ sickchill/views/config/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from anime import ConfigAnime
+from backup import ConfigBackupRestore
+from index import Config
+from notifications import ConfigNotifications
+from post_processing import ConfigPostProcessing
+from providers import ConfigProviders
+from search import ConfigSearch
+from shares import ConfigShares
+from subtitles import ConfigSubtitles
---
+++ sickchill/views/manage/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+
+from add_shows import AddShows
+from index import Manage
+from post_process import PostProcess
+from searches import ManageSearches
